4d0478b2a8557fc6239b739ea7ed3109218b0e03
Hitomi Miyazaki <dayan0211hm@gmail.com>
2020-06-08 01:34:59 +0900

    制御できない

--- docs/js/index.js
+++ docs/js/index.js
@@ -3,34 +3,21 @@ const id = '1BpGnuwC4lZf9G2yFyiSrxbJuGO8gviV8mr-I2D3x4vA';
 const sheet = 'studio';
 const endpoint = `${uri}?id=${id}&sheet=${sheet}`;
 
+//let mode = 1;
+
 const renderJson = (json) => {
   const studios = json.records;
   studios.forEach(studio => {
     const studioDiv = document.createElement('div');
     const studioTitle = document.createElement("span");
-    //const studioDisply = document.createElement("style");
     studioTitle.className = 'studio-title';
     studioTitle.textContent = studio['name-ja'];
 
     const studioTitleEn = document.createElement("span");
-    //const studioDisplyEn = document.createElement("style");
     studioTitleEn.className = 'studio-title-en';
     studioTitleEn.textContent = studio['name-en'];
     studioTitle.style.display = "block";
     studioTitleEn.style.display = "none";
-
-    document.getElementById("button").onclick = function () {
-      var langBotton = document.getElementById("button");
-      var langBottonClass = langBotton.getAttribute("class");
-
-      if (langBottonClass == "open") {
-        studioTitle.style.display = "none";
-        studioTitleEn.style.display = "block";
-      } else {
-        studioTitle.style.display = "block";
-        studioTitleEn.style.display = "none";
-      }
-    }
 
     studioDiv.appendChild(studioTitle);
     studioDiv.appendChild(studioTitleEn);
@@ -39,20 +26,7 @@ const renderJson = (json) => {
   document.getElementById('result').textContent = JSON.stringify(json, null, 2);
 }
 
-const getData = async () => {
-  try {
-    const response = await fetch(endpoint);
-    if (response.ok) {
-      let jsonResponse = await response.json();
-      renderJson(jsonResponse);
-    }
-  }
-  catch (error) {
-    console.log(error);
-  }
-}
 
-getData();
 
 
 
@@ -77,8 +51,10 @@ document.getElementById("button").onclick = function () {
     document.getElementById('nomalText').style.display = "none";
     document.getElementById('englishText').style.display = "block";
 
-    document.getElementsById('studio-title').style.display = "none";
-    document.getElementsById('studio-title-en').style.display = "block";
+
+    document.getElementsByClassName('studio-title').style.display = "none";
+    document.getElementsByClassName('studio-title-en').style.display = "block";
+    // mode = 2;
 
   } else {
     langBotton.classList.remove('close');
@@ -97,9 +73,24 @@ document.getElementById("button").onclick = function () {
     document.getElementById('nomalText').style.display = "block";
     document.getElementById('englishText').style.display = "none";
 
-    document.getElementsById('studio-title').style.display = "block";
-    document.getElementsById('studio-title-en').style.display = "none";
-
+    document.getElementsByClassName('studio-title').style.display = "block";
+    document.getElementsByClassName('studio-title-en').style.display = "none";
+    //mode = 1;
   }
 
 };
+
+const getData = async () => {
+  try {
+    const response = await fetch(endpoint);
+    if (response.ok) {
+      let jsonResponse = await response.json();
+      renderJson(jsonResponse);
+    }
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+getData();

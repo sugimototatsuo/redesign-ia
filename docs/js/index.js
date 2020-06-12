bfda5323ef03457783dddd1445150f1605bc1a20
YUIKO SUYAMA <Sumikko-Mountain@users.noreply.github.com>
2020-06-12 15:37:29 +0900

    最後の要素の取り除きと　jsonでアニメーションできそうなののテスト途中

--- docs/js/index.js
+++ docs/js/index.js
@@ -9,38 +9,42 @@ const endpoint3 = `${uri}?id=${id}&sheet=${sheet3}`;
 //jsonをHTMLにとってきて表示するのに必要な基本的な流れは①~⑤である
 const renderJson = (json) => {
   const studios = json.records;
-  studios.forEach(studio => {
-    const studioDiv = document.createElement('div');
+  studios.pop();//配列の一番最後の要素を取り出すメソッド　ほんとは返り値があって const lastStudio =  studios.pop();とかするべきなんだけど、さいごが空ってわかってるのでOK
+  studios.forEach(studio => {//forEachで各スタジオをグルグル回してる　配列が12個だとちょうどいいのに13個あるので、最後の要素を取ってあげればいい
 
-    const studioTitle = document.createElement("span");//①studioTitle　という変数でもって、span要素の作成
-    studioTitle.className = 'studio-title';//②span要素？にクラスの名前をつけてCSSで指定できるようにしている
-    studioTitle.textContent = studio['name-ja'];//③[データシート上のkey]
+    if (studio['name-ja'] !== '') {
+      const studioDiv = document.createElement('div');
 
-    const studioTitleEn = document.createElement("span");//新たにspan要素の作成　-jaと同上  
-    studioTitleEn.className = 'studio-title-en';
-    studioTitleEn.textContent = studio['name-en'];
+      const studioTitle = document.createElement("span");//①studioTitle　という変数でもって、span要素の作成
+      studioTitle.className = 'studio-title';//②span要素？にクラスの名前をつけてCSSで指定できるようにしている
+      studioTitle.textContent = studio['name-ja'];//③[データシート上のkey]
 
-    const studioCore = document.createElement("span");
-    studioCore.className = 'studio-core';
-    studioCore.textContent = studio['core-ja'];
+      const studioTitleEn = document.createElement("span");//新たにspan要素の作成　-jaと同上  
+      studioTitleEn.className = 'studio-title-en';
+      studioTitleEn.textContent = studio['name-en'];
 
-    studioDiv.appendChild(studioTitle);//④
-    studioDiv.appendChild(studioTitleEn);
-    studioDiv.appendChild(studioCore);
+      const studioCore = document.createElement("span");
+      studioCore.className = 'studio-core';
+      studioCore.textContent = studio['core-ja'];
 
-    document.getElementById('studios').appendChild(studioDiv);//⑤HTML上のstudiosというidがついたdivにここまでの作業で作ったsudioDivを追加
+      studioDiv.appendChild(studioTitle);//④
+      studioDiv.appendChild(studioTitleEn);
+      studioDiv.appendChild(studioCore);
+
+      document.getElementById('studios').appendChild(studioDiv);//⑤HTML上のstudiosというidがついたdivにここまでの作業で作ったsudioDivを追加
 
 
-    const facultyDiv = document.createElement('div');
+      const facultyDiv = document.createElement('div');
 
-    const facultyName = document.createElement("span");
-    facultyName.className = 'faculty-name';
-    facultyName.textContent = studio['f-faculty-ja'];
+      const facultyName = document.createElement("span");
+      facultyName.className = 'faculty-name';
+      facultyName.textContent = studio['f-faculty-ja'];
 
-    facultyDiv.appendChild(facultyName);//④
+      facultyDiv.appendChild(facultyName);//④
 
-    document.getElementById('faculty').appendChild(facultyDiv);
+      document.getElementById('faculty').appendChild(facultyDiv);
 
+    }
   });
   document.getElementById('result').textContent = JSON.stringify(json, null, 2);//JavaScript のオブジェクトや値を JSON 文字列に変換させて、HTML上のresultというidがついたdivに付与？する。最後の引数は上限が10のスペーシング 変わってんのかわからない
 }

891b0cbf461b67cec8dd80181974b27d6d92c24d
Hitomi Miyazaki <dayan0211hm@gmail.com>
2020-06-29 00:54:35 +0900

    form

--- docs/js/work.js
+++ docs/js/work.js
@@ -3,35 +3,83 @@ var langMode = 0;
 
 window.onload = function () {
     var langBotton = document.getElementById("button");
+    var langBotton2 = document.getElementById("button2");
 
     if (document.URL.match("/?lang=0")) {
 
     }
     else if (document.URL.match("/?lang=1")) {
+        langMode = 1;
         langBotton.classList.remove('open');
         langBotton.classList.add('close');
-
+        //langBottonWork.classList.remove('open');
+        //langBottonWork.classList.add('close');
         for (var i = 0; i <= 8; i++) {
             document.getElementById('nomalSubtitle' + (i + 1).toString()).style.display = "none";
             document.getElementById('easySubtitle' + (i + 1).toString()).style.display = "none";
             document.getElementById('englishSubtitle' + (i + 1).toString()).style.display = "block";
         }
 
+        for (var i = 0; i <= 2; i++) {
+            document.getElementById('nomalGrade' + (i + 1).toString()).style.display = "none";
+            document.getElementById('easyGrade' + (i + 1).toString()).style.display = "none";
+            document.getElementById('englishGrade' + (i + 1).toString()).style.display = "block";
+        }
+
+
         document.getElementById('nomalButton').style.display = "none";
         document.getElementById('englishButton').style.display = "block";
+        document.getElementById('nomalButton2').style.display = "block";
+        document.getElementById('easyButton2').style.display = "none";
 
         document.getElementById('nomalTitle').style.display = "none";
         document.getElementById('englishTitle').style.display = "block";
+        document.getElementById('easyTitle').style.display = "none";
 
         document.getElementById('nomalCap').style.display = "none";
         document.getElementById('englishCap').style.display = "block";
+        document.getElementById('easyCap').style.display = "none";
 
         document.getElementById('nomalReturn').style.display = "none";
         document.getElementById('englishReturn').style.display = "block";
     }
+    else if (document.URL.match("/?lang=2")) {
+        langMode = 2;
+        langBotton2.classList.remove('open');
+        langBotton2.classList.add('close');
+        //langBottonWork.classList.remove('open');
+        //langBottonWork.classList.add('close');
+        for (var i = 0; i <= 8; i++) {
+            document.getElementById('nomalSubtitle' + (i + 1).toString()).style.display = "none";
+            document.getElementById('easySubtitle' + (i + 1).toString()).style.display = "block";
+            document.getElementById('englishSubtitle' + (i + 1).toString()).style.display = "none";
+        }
+
+        for (var i = 0; i <= 2; i++) {
+            document.getElementById('nomalGrade' + (i + 1).toString()).style.display = "none";
+            document.getElementById('easyGrade' + (i + 1).toString()).style.display = "block";
+            document.getElementById('englishGrade' + (i + 1).toString()).style.display = "none";
+        }
+
+        document.getElementById('nomalButton').style.display = "block";
+        document.getElementById('englishButton').style.display = "none";
+        document.getElementById('nomalButton2').style.display = "none";
+        document.getElementById('easyButton2').style.display = "block";
+
+        document.getElementById('nomalTitle').style.display = "none";
+        document.getElementById('englishTitle').style.display = "none";
+        document.getElementById('easyTitle').style.display = "block";
+
+        document.getElementById('nomalCap').style.display = "none";
+        document.getElementById('englishCap').style.display = "none";
+        document.getElementById('easyCap').style.display = "block";
+
+        document.getElementById('nomalReturn').style.display = "block";
+        document.getElementById('englishReturn').style.display = "none";
+    }
 };
 
-
+//言語切り替え
 document.getElementById("button").onclick = function () {
     var langBotton = document.getElementById("button");
     var langBottonClass = langBotton.getAttribute("class");
@@ -50,18 +98,28 @@ document.getElementById("button").onclick = function () {
             document.getElementById('englishSubtitle' + (i + 1).toString()).style.display = "block";
         }
 
+        for (var i = 0; i <= 2; i++) {
+            document.getElementById('nomalGrade' + (i + 1).toString()).style.display = "none";
+            document.getElementById('easyGrade' + (i + 1).toString()).style.display = "none";
+            document.getElementById('englishGrade' + (i + 1).toString()).style.display = "block";
+        }
+
 
         document.getElementById('nomalButton').style.display = "none";
         document.getElementById('englishButton').style.display = "block";
+        document.getElementById('nomalButton2').style.display = "block";
+        document.getElementById('easyButton2').style.display = "none";
         //document.getElementById('nomalButtonWork').style.display = "none";
         //document.getElementById('englishButtonWork').style.display = "block";
 
 
         document.getElementById('nomalTitle').style.display = "none";
         document.getElementById('englishTitle').style.display = "block";
+        document.getElementById('easyTitle').style.display = "none";
 
         document.getElementById('nomalCap').style.display = "none";
         document.getElementById('englishCap').style.display = "block";
+        document.getElementById('easyCap').style.display = "none";
 
         document.getElementById('nomalReturn').style.display = "none";
         document.getElementById('englishReturn').style.display = "block";
@@ -75,6 +133,8 @@ document.getElementById("button").onclick = function () {
         //langBottonWork.classList.add('open');
         document.getElementById('nomalButton').style.display = "block";
         document.getElementById('englishButton').style.display = "none";
+        document.getElementById('nomalButton2').style.display = "block";
+        document.getElementById('easyButton2').style.display = "none";
 
         for (var i = 0; i <= 8; i++) {
             document.getElementById('nomalSubtitle' + (i + 1).toString()).style.display = "block";
@@ -82,12 +142,20 @@ document.getElementById("button").onclick = function () {
             document.getElementById('englishSubtitle' + (i + 1).toString()).style.display = "none";
         }
 
+        for (var i = 0; i <= 2; i++) {
+            document.getElementById('nomalGrade' + (i + 1).toString()).style.display = "block";
+            document.getElementById('easyGrade' + (i + 1).toString()).style.display = "none";
+            document.getElementById('englishGrade' + (i + 1).toString()).style.display = "none";
+        }
+
 
         document.getElementById('nomalTitle').style.display = "block";
         document.getElementById('englishTitle').style.display = "none";
+        document.getElementById('easyTitle').style.display = "none";
 
         document.getElementById('nomalCap').style.display = "block";
         document.getElementById('englishCap').style.display = "none";
+        document.getElementById('easyCap').style.display = "none";
 
         document.getElementById('nomalReturn').style.display = "block";
         document.getElementById('englishReturn').style.display = "none";
@@ -110,6 +178,87 @@ document.getElementById("button").onclick = function () {
         });
     }
 
+};
+
+document.getElementById("button2").onclick = function () {
+    var langBotton2 = document.getElementById("button2");
+    var langBottonClass2 = langBotton2.getAttribute("class");
+    //var langBottonWork = document.getElementById("buttonWork");
+    //var langBottonWorkClass = langBotton.getAttribute("class");
+
+    if (langBottonClass2 == "open") {
+        langMode = 2;
+        langBotton2.classList.remove('open');
+        langBotton2.classList.add('close');
+        //langBottonWork.classList.remove('open');
+        //langBottonWork.classList.add('close');
+        for (var i = 0; i <= 8; i++) {
+            document.getElementById('nomalSubtitle' + (i + 1).toString()).style.display = "none";
+            document.getElementById('easySubtitle' + (i + 1).toString()).style.display = "block";
+            document.getElementById('englishSubtitle' + (i + 1).toString()).style.display = "none";
+        }
+
+        for (var i = 0; i <= 2; i++) {
+            document.getElementById('nomalGrade' + (i + 1).toString()).style.display = "none";
+            document.getElementById('easyGrade' + (i + 1).toString()).style.display = "block";
+            document.getElementById('englishGrade' + (i + 1).toString()).style.display = "none";
+        }
+
+        document.getElementById('nomalButton').style.display = "block";
+        document.getElementById('englishButton').style.display = "none";
+        document.getElementById('nomalButton2').style.display = "none";
+        document.getElementById('easyButton2').style.display = "block";
+        //document.getElementById('nomalButtonWork').style.display = "none";
+        //document.getElementById('englishButtonWork').style.display = "block";
+
+
+        document.getElementById('nomalTitle').style.display = "none";
+        document.getElementById('englishTitle').style.display = "none";
+        document.getElementById('easyTitle').style.display = "block";
+
+        document.getElementById('nomalCap').style.display = "none";
+        document.getElementById('englishCap').style.display = "none";
+        document.getElementById('easyCap').style.display = "block";
+
+        document.getElementById('nomalReturn').style.display = "block";
+        document.getElementById('englishReturn').style.display = "none";
+
+
+    } else if (langBottonClass2 == "close") {
+        langMode = 0;
+        langBotton2.classList.remove('close');
+        langBotton2.classList.add('open');
+        //langBottonWork.classList.remove('close');
+        //langBottonWork.classList.add('open');
+        document.getElementById('nomalButton').style.display = "block";
+        document.getElementById('englishButton').style.display = "none";
+        document.getElementById('nomalButton2').style.display = "block";
+        document.getElementById('easyButton2').style.display = "none";
+
+        for (var i = 0; i <= 8; i++) {
+            document.getElementById('nomalSubtitle' + (i + 1).toString()).style.display = "block";
+            document.getElementById('easySubtitle' + (i + 1).toString()).style.display = "none";
+            document.getElementById('englishSubtitle' + (i + 1).toString()).style.display = "none";
+        }
+
+        for (var i = 0; i <= 2; i++) {
+            document.getElementById('nomalGrade' + (i + 1).toString()).style.display = "block";
+            document.getElementById('easyGrade' + (i + 1).toString()).style.display = "none";
+            document.getElementById('englishGrade' + (i + 1).toString()).style.display = "none";
+        }
+
+        document.getElementById('nomalTitle').style.display = "block";
+        document.getElementById('englishTitle').style.display = "none";
+        document.getElementById('easyTitle').style.display = "none";
+
+        document.getElementById('nomalCap').style.display = "block";
+        document.getElementById('englishCap').style.display = "none";
+        document.getElementById('easyCap').style.display = "none";
+
+        document.getElementById('nomalReturn').style.display = "block";
+        document.getElementById('englishReturn').style.display = "none";
+
+    }
 };
 
 //headerのページ遷移時と言語情報渡し

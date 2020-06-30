c2ba0c175c1724a926a381ed3bff0afd1be3eb39
YUIKO SUYAMA <Sumikko-Mountain@users.noreply.github.com>
2020-06-30 10:29:27 +0900

    header

--- docs/js/home.js
+++ docs/js/home.js
@@ -219,37 +219,6 @@ window.onload = function () {
 
 
 
-
-/*---------topへ戻るボタン-------------*/
-function getScrolled() {
-    return (window.pageYOffset !== undefined) ? window.pageYOffset : document.documentElement.scrollTop;
-}
-
-//トップに戻るボタンの要素を取得
-var topButton = document.getElementById('js-scroll-fadein');
-
-//ボタンの表示・非表示
-window.onscroll = function () {
-    (getScrolled() > 500) ? topButton.classList.add('is-fadein') : topButton.classList.remove('is-fadein');
-};
-
-//トップに移動する関数
-function scrollToTop() {
-    var scrolled = getScrolled();
-    window.scrollTo(0, Math.floor(scrolled / 2));
-    if (scrolled > 0) {
-        window.setTimeout(scrollToTop, 30);
-    }
-};
-
-//イベント登録
-topButton.onclick = function () {
-    scrollToTop();
-};
-
-
-
-
 /*----------------スタジオ画像のスライドショ--------------- ref:http://cya.sakura.ne.jp/java/sshow2.htm*/
 
 var slideCount = -1;		// 現在表示している配列番号
